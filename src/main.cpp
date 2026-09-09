b60304b342b2710ac293ad674e1eeb05f316d109
Gerardo Avalos <gerhard.avsn0901@gmail.com>
2026-09-09 17:23:04 -0600

    added POO for Input validator and menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include "menu.hpp"
+#include <iostream>
+#include "Menu.hpp"
 
 /**
  * @brief Program entry point.
@@ -7,7 +8,28 @@
  */
 int main(void)
 {
-    menuSelection(displayMenu());
-
+    Menu menu({"Create a task", "Read task/s", "Update task", "Delete a task", "Exit"});
+ 
+    int selectedOption = menu.displayMenu();
+ 
+    switch (selectedOption)
+    {
+        case 1:
+            std::cout << "-> Create a task" << std::endl;
+            break;
+        case 2:
+            std::cout << "-> Read task/s" << std::endl;
+            break;
+        case 3:
+            std::cout << "-> Update task" << std::endl;
+            break;
+        case 4:
+            std::cout << "-> Delete a task" << std::endl;
+            break;
+        case 5:
+            std::cout << "Exiting..." << std::endl;
+            break;
+    }
+ 
     return 0;
 }
